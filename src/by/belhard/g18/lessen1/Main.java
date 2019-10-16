7f9d30d747d91212bcaf22818c183acb56a6c027
Sergey Bindel <Bindel1996@mail.ru>
2019-10-17 00:42:44 +0300

    method main was added

--- src/by/belhard/g18/lessen1/Main.java
+++ src/by/belhard/g18/lessen1/Main.java
@@ -1,8 +1,14 @@
 package by.belhard.g18.lessen1;
 
 public class Main {
-}
 
-    public static void mgit cain(String[] args) {
+    public static void main(String[] args) {
+        int i = 158;
+        System.out.println(i * 30 % 24);
+        System.out.println(i>=100);
+
 
     }
+}
+
+
