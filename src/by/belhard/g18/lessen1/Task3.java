b335a994a9e43d1c9f486e769f58a04bd27cc425
Sergey Bindel <Bindel1996@mail.ru>
2019-10-17 15:29:22 +0300

    signing with words

--- src/by/belhard/g18/lessen1/Task3.java
+++ src/by/belhard/g18/lessen1/Task3.java
@@ -4,16 +4,22 @@ public class Task3
 {
     public static void main(String[] args) {
         int x1 = 5;
-        int x = 6;
         int y1 = 8;
         int x2 = 13;
         int y2 = 11;
-        int l = ((x*x2-x*x1)*(x*x2-x*x1)+(y2-y1)*(y2-y1));
+        int l = ((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
         System.out.println(l);
         int rad = 9;
-        System.out.println(l < rad);
-        System.out.println(l > rad);
-        System.out.println(l == rad);
+        //System.out.println(l < rad);
+        if (l < rad) {
+            System.out.println("Inside");
+        } else if (l > rad) {
+            System.out.println("Outside");
+        } else {
+            System.out.println("On the round");
+        }
+        //System.out.println(l > rad);
+        //System.out.println(l == rad);
 
     }
 }
