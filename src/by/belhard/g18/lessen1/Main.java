37b013bd5af9959e5a4176234b5be83f5140e28e
Sergey Bindel <Bindel1996@mail.ru>
2019-10-17 13:28:19 +0300

    completed the calculation task

--- src/by/belhard/g18/lessen1/Main.java
+++ src/by/belhard/g18/lessen1/Main.java
@@ -5,7 +5,43 @@ public class Main {
     public static void main(String[] args) {
         int i = 158;
         System.out.println(i * 30 % 24);
-        System.out.println(i>=100);
+        System.out.println(i >= 100);
+        int b = 187;
+        System.out.println(i / b + 1);
+        System.out.println(i==b);
+        System.out.println(i!=b);
+        int s = 33;
+        System.out.println(s++);
+        System.out.println(s);
+        System.out.println(~s);
+        System.out.println(158&187);
+        System.out.println(158^187);
+        int e = 357;
+        int t = 459;
+        System.out.println(e&t);
+        int z = (t << e);
+        System.out.println(z);
+        System.out.println(111111111 << 3);
+        int a = (t >> e);
+        System.out.println(a);
+        System.out.println(35 >> 2);
+        int l = 6- 4/2;
+        System.out.println(l);
+        int r = 10;
+        System.out.println(r);
+        double p = 3.14;
+        System.out.println(p * (r * r)); // Рфдиус окружности
+        System.out.println(2* p * r); // Длина окружности
+        String v = "корректный ввод" ;
+        System.out.println(v);
+
+        int RX = 7;
+        int RY = 8;
+        int
+
+
+
+
 
 
     }
