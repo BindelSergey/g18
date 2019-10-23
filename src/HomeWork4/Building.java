90a701c1f489b3afee2a3eca7a103ff79ccad3f5
Sergey Bindel <Bindel1996@mail.ru>
2019-10-23 19:14:32 +0300

    coment

--- src/HomeWork4/Building.java
+++ src/HomeWork4/Building.java
@@ -2,14 +2,9 @@ package HomeWork4;
 
 public class Building {
     public int number_of_floors = 8;
-    public String address = "kalinka";{
-        System.out.println(address);
-    }
-    public boolean elevator; {
-       } public String material = "kirpih"; {
-        System.out.println(material);
-
-        }
+    public String address = "kalinka";
+    public boolean elevator;
+    public String material = "kirpih";
     public int year_of_construction = 2013;
-    }
+}
 
