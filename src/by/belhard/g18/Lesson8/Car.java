500f8d0d167d8c97367a2ac59b6a6fe82422504d
Bindel Sergey <Bindel1996@mail.ru>
2019-11-05 21:23:59 +0300

    com

---
+++ src/by/belhard/g18/Lesson8/Car.java
@@ -0,0 +1,14 @@
+package by.belhard.g18.Lesson8;
+
+public class Car {
+
+    private String title;
+    private  WheelType wheels;
+    private FuelTupe fuel;
+
+    public Car(String title, WheelType wheels, FuelTupe fuel) {
+        this.title = title;
+        this.wheels = wheels;
+        this.fuel = fuel;
+    }
+}
